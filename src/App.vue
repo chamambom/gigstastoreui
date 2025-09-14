c35e414da7c203bd25dbea361d786a313ab93662
Martin Chamambo <chamambom@gmail.com>
2025-09-14 18:14:37 +1200

    updated welcome.vue page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <script setup>
-import HelloWorld from './components/Welcome.vue'
+import Welcome from './components/Welcome.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<!--  <div>-->
+<!--    <a href="https://vite.dev" target="_blank">-->
+<!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
+<!--    </a>-->
+<!--    <a href="https://vuejs.org/" target="_blank">-->
+<!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
+<!--    </a>-->
+<!--  </div>-->
 </template>
 
 <style scoped>
